5ae87a096b257a0ccb2cf875ab108b90abc0a238
Magomed Mamergov <m.mamergovv@yandex.ru>
2025-03-22 17:52:51 +0300

    initialized project

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import './style.css'
+import typescriptLogo from './typescript.svg'
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <div>
+
+    <a href="https://www.typescriptlang.org/" target="_blank">
+      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
+    </a>
+    <h1>Explore algorithms and data structures with TS</h1>
+ 
+  </div>
+`
+
+document.querySelector<HTMLButtonElement>('#counter')
